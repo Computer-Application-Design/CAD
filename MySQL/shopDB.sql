36eeaf6405797a4760c064948b87017540e1a5ae
khsexk <kohyunsuk98@gmail.com>
2021-12-06 19:56:14 +0900

    mysql - shopdb 스키마에 manager table 생성

---
+++ MySQL/shopDB.sql
@@ -0,0 +1,8 @@
+create table `shopdb`.`manager` (
+	id VARCHAR(20) NOT NULL Primary Key,
+	passwd VARCHAR(20) NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	depart VARCHAR(20) NOT NULL,
+	mobile VARCHAR(15),
+	email VARCHAR(30)
+);
