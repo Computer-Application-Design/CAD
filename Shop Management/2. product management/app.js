bc1196cb6fbe6d3f1b7d156bcd73ea19be2ea198
khsexk <kohyunsuk98@gmail.com>
2021-12-08 12:29:12 +0900

    app.js 다시 생성 및 pug 모듈 install

---
+++ Shop Management/2. product management/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path')
+
+
+const adminData = require("./routes/admin");
+const shopRoutes = require('./routes/shop');
+
+const Port = 3001;
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views','views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes);
+app.use( shopRoutes);
+
+
+app.use('*', (req, res, next ) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+
+
+app.listen( Port, ( ) => {
+    console.log(`Server is running on port: ${Port}`);
+})
