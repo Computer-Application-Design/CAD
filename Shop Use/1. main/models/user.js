fec5a2dd62450faaf33305a51caf7c6b4e33be56
khsexk <kohyunsuk98@gmail.com>
2021-12-08 20:01:19 +0900

    user 회원가입 로그인 commit

---
+++ Shop Use/1. main/models/user.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      id: {
+        type: Sequelize.STRING(40),
+        allowNull: true,
+        unique: true,
+        primaryKey: true
+      },
+      passwd: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+      },
+      username: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      mobile: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(30),
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: false,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'manager',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+};
